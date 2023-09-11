ae697dffc7a86d3d017c564eb9642c1262044988
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2023-09-11 15:01:30 +0330

    Add files via upload

---
+++ Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Monopoli
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+        public int Numder { get; set; }
+        public string RoomName { get; set; }
+        public string Password { get; set; }
+        public int Count { get; set; }
+        public int Coin { get; set; }
+        public int PreBuy { get; set; }
+        public bool IsStart { get; set; }
+        public string Home { get; set; }
+    }
+}
