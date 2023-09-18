4a6a933377253640671918e6cd1a726689f7fbe3
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2023-09-18 23:25:34 +0330

    Add files via upload

---
+++ Place.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Monopoli
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public int Player { get; set; }
+        public int Tower { get; set; }
+    }
+}
