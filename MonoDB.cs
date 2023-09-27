cb679c5008a8bd1487de4dc340eca1775c64ea25
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2023-09-27 15:48:43 +0330

    Add files via upload

---
+++ MonoDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Monopoli
+{
+    public class MonoDB : DbContext
+    {
+        public MonoDB(DbContextOptions<MonoDB> options) : base(options) { }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Place> Places { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Room>().Property("Id").ValueGeneratedNever();
+            builder.Entity<Player>().Property("Id").ValueGeneratedNever();
+        }
+    }
+}
